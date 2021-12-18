2a5fb3bddbb819362326db064c5ebcacb6157a19
N-Gibson <noah.gibson99@gmail.com>
2021-12-17 19:10:57 -0700

    '

--- index.js
+++ index.js
@@ -1,22 +1,37 @@
 const Nightmare = require ('nightmare');
 const nightmare = Nightmare({ show: true });
+require('dotenv').config()
+
+function getTodaysDate() {
+  const today = new Date()
+  const month = today.getMonth()
+  const day = today.getDay()
+  const year = today.getFullYear()
+
+  return `${month}/${day}/${year}`
+}
+
+function filterByDay() {
+  const timeLogs = document.querySelectorAll('tr')
+  const todaysDate = getTodaysDate()
+
+  const todaysLogs = [...timeLogs].filter(timeLog => timeLog.childNodes[2].innerHTML === todaysDate)
+
+  return todaysLogs
+}
+
+function evaluate() {
+
+}
 
 nightmare
-  .goto('www.https://servicio.tech-niche.net')
+  .goto('https://servicio.tech-niche.net/#/login?redirect=%2F')
   .type('#email', process.env.EMAIL)
   .type('#password', process.env.PW)
   .click('.btn-primary')
-  .wait('.result')
-  .evaluate(() => {
-    let timeLog = document.querySelectorAll('td');
-
-    // let jobTitles = document.querySelectorAll('.title')
-    // let list = [].slice.call(jobTitles)
-    // return list.map((node) => {
-    //   return node.innerText
-    // })
-    return timeLog
-  })
-  .end()
+  .wait('.alert')
+  .click('.btn-primary')
+  .evaluate(elavuate())
+  // .end()
   .then(result => console.log(result))
   .catch(err => console.error(err))
